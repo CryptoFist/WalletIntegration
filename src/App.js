69a4475b968851bb21edf8937d8d9a3de1d9ce9b
CryptoFist <kelldaniel1002@gmail.com>
2024-12-09 14:06:08 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
 import "./App.css";
+import "@rainbow-me/rainbowkit/styles.css";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useEffect, useState } from "react";
+import { useDisconnect } from "wagmi";
 
 function App() {
+    const { disconnect } = useDisconnect();
+    const [address, setAddress] = useState("");
+    const [isConnected, setConnected] = useState(false);
     return (
         <div className="App">
-            <ConnectButton />
+            <ConnectButton.Custom>
+                {({
+                    account,
+                    chain,
+                    openAccountModal,
+                    openChainModal,
+                    openConnectModal,
+                    authenticationStatus,
+                    connectModalOpen,
+                    mounted,
+                }) => {
+                    const handleWalletConnect = () => {
+                        if (isConnected == false) {
+                            openConnectModal();
+                        } else {
+                            disconnect();
+                        }
+                    };
+                    const ready = mounted && authenticationStatus !== "loading";
+                    const connected =
+                        ready &&
+                        account &&
+                        chain &&
+                        (!authenticationStatus ||
+                            authenticationStatus === "authenticated");
+
+                    setConnected(connected === true ? true : false);
+                    setAddress(connected === true ? account.address : "");
+
+                    if (connected === true) {
+                        return (
+                            <div>
+                                <button onClick={() => handleWalletConnect()}>
+                                    Disconnect Wallet
+                                </button>
+                                <p class="txt-address">{address}</p>
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div>
+                                <button onClick={() => handleWalletConnect()}>
+                                    Connect Wallet
+                                </button>
+                                <p class="txt-address">{address}</p>
+                            </div>
+                        );
+                    }
+                }}
+            </ConnectButton.Custom>
         </div>
     );
 }
--- src/wagmi.js
+++ src/wagmi.js
@@ -3,6 +3,6 @@ import { mainnet } from "viem/chains";
 
 export const config = getDefaultConfig({
     appName: "RainbowKit demo",
-    projectId: "WalletIntegration",
+    projectId: "5f7ad8eb164baabb82e245bf09ba1202",
     chains: [mainnet],
 });
